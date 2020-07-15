c7205175cf45ffb93c1cee53a8a8625064dea632
Atchess <A22Q7777777@gmail.com>
2020-07-15 20:05:59 +0800

    重新开始

--- miniTiktok/app/src/main/java/com/example/minitiktok/base/BaseRvViewHolder.java
+++ miniTiktok/app/src/main/java/com/example/minitiktok/base/BaseRvViewHolder.java
@@ -1,14 +1,4 @@
 package com.example.minitiktok.base;
 
-import android.view.View;
-import androidx.recyclerview.widget.RecyclerView;
-import butterknife.ButterKnife;
-
-
-public class BaseRvViewHolder extends RecyclerView.ViewHolder {
-
-    public BaseRvViewHolder(View itemView) {
-        super(itemView);
-        ButterKnife.bind(this, itemView);
-    }
+public class BaseRvViewHolder {
 }
